7bba0c8860728823bcc5924da7bf54f4b8357db1
Vishal-S-P <86377748+Vishal-S-P@users.noreply.github.com>
2024-02-18 11:46:43 +0000

    Deploying to gh-pages from @ Vishal-S-P/vishal-s-p.github.io@0347f679265ff306bfbc1fbef2e86abfcdcae06c 🚀

--- projects/projects.js
+++ projects/projects.js
@@ -4,14 +4,14 @@ const projects = [
     { 
         title: "Project 1",
         imageUrl: "https://via.placeholder.com/150",
-        description: "Description for Project 1",
-        url: "https://vishal-s-p.github.io/projects/2023/proj_1.html"
+        url: "https://vishal-s-p.github.io/projects/2023/proj_1.html",
+        abstract: "Good learning experience."
     },
     { 
         title: "Project 2",
         imageUrl: "https://via.placeholder.com/150",
-        description: "Description for Project 2",
-        url: "https://vishal-s-p.github.io/projects/2023/proj_2.html"
+        url: "https://vishal-s-p.github.io/projects/2023/proj_2.html",
+        
     },
     
 ];
@@ -37,12 +37,21 @@ function createProjectBlocks() {
         projectImage.classList.add("project-image");
         projectImage.src = project.imageUrl;
 
-        const projectDescription = document.createElement("div");
-        projectDescription.classList.add("project-description");
-        projectDescription.textContent = project.description;
+        // const projectDescription = document.createElement("div");
+        // projectDescription.classList.add("project-description");
+        // projectDescription.textContent = project.description;
+
+        // Abstract in italics
+        const projectAbstract = document.createElement("div");
+        projectAbstract.classList.add("project-abstract");
+        // Create an italic element to wrap the abstract text
+        const italicText = document.createElement("i");
+        italicText.textContent = project.abstract; // Use project.abstract or similar
+        projectAbstract.appendChild(italicText);
+
 
         projectContent.appendChild(projectTitle);
-        projectContent.appendChild(projectDescription);
+        projectContent.appendChild(projectAbstract);
 
         projectBlock.appendChild(projectImage);
         projectBlock.appendChild(projectContent);
